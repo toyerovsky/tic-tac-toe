0ec2193a67016c32f143266f5998e804464d1d1e
toyerovsky <przemyslaw.postrach@hotmail.com>
2018-05-04 15:57:24 +0200

    Add project structure, skybox with rotation

---
+++ Assets/Scripts/SkyboxRotationScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class SkyboxRotationScript : MonoBehaviour
+    {
+        public float RotationSpeed;
+        // Update is called once per frame
+        void Update()
+        {
+            GetComponent<Skybox>().material.SetFloat("_Rotation", Time.time * RotationSpeed);
+        }
+    }
+}
